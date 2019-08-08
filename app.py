9cd01520a80ba66124397fa65a223b8930d1d208
ayang <nofate.yang@hotmail.com>
2019-08-08 20:31:13 +0800

    change words style.

--- app.py
+++ app.py
@@ -5,12 +5,16 @@
 import os
 import platform
 import requests
-from urllib import parse
+import textwrap
 
+from urllib import parse
+from bs4 import BeautifulSoup
 from easydict import EasyDict as edict
+
 # from hobee import RequestX as Request
 
 from PIL import Image, ImageFont, ImageDraw
+
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8;',
@@ -27,7 +31,8 @@ else:
     local_root = os.environ['HOME'] + '/Wallpapers'
 
 
-log_file = local_root + '/bing-wallpaper.log'
+logs = []
+log_file = local_root + '/bing-wallpaper.txt'
 
 
 def debug_print(message):
@@ -76,20 +81,73 @@ def notify(filename, title):
 
 
 def draw_text_info_to_image(image_file_name, image_info):
-    font = ImageFont.truetype("font/SourceHanSansSC-Regular.otf", 48)
-
+    
+    words = []
+    
+    # 每日一言
+    url = 'http://guozhivip.com/nav/api/api.php'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200 and len(resp.content) > 0:
+        result = resp.content.decode('utf-8')
+        result = result.replace('document.write("', '').replace('");', '') 
+        if len(result) > 0:
+            words.append(result) 
+    
+    # 每日一句
+    url = 'http://guozhivip.com/'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200 and len(resp.content) > 0:
+        result = resp.content.decode('utf-8')
+        # debug_print(result)
+        soup = BeautifulSoup(result, "html.parser")
+        result = soup.select('body > div.contents > section.com.one > div.sub.content > ul > li.words > h3')
+        if result is not None and len(result) > 0:
+            # debug_print(str(result[0]))
+            result = str(result[0]).replace('<h3>', '').replace('</h3>', '') # 这里没有办法，因为需要用到<br/>来分行
+            results = result.split('<br/>')
+            for text in results:
+                words.append(text.strip())
+            
+        result = soup.select('body > div.contents > section.com.one > div.sub.content > ul > li.words > p')
+        if result is not None and len(result) > 0:
+            result = result[0].text.replace('果汁小编：', '')
+            words.append(result)
+            # debug_print(result)
+    
+    logs.extend(words)
+    debug_print('======================================')
+    
     im = Image.open(image_file_name)
     print(im.format, im.size, im.mode)
 
-    x = 40
-    y = im.size[1] - 240
-    text_info = image_info.copyright.split('，')
+    iw, ih = im.size[0], im.size[1]
     
-    draw = ImageDraw.Draw(im)
+    x = 220
+    y = 20
+   
+    draw = ImageDraw.Draw(im) 
+   
+    y = ih - 350
+    t_height = 0
+    font = ImageFont.truetype("font/SourceHanSansSC-Regular.otf", 24)
+    for index,text in enumerate(words):
+        w, h = font.getsize(text)
+        t_height = t_height + h
+    
+    w_h = t_height / len(words) 
+    for index,text in enumerate(words):
+        y = y + w_h + 0
+        debug_print(text + ' %d, %d ' % (w, h))
+        draw.text((x, y), text, fill=(255, 255, 255),  font=font)
+    
+    y = y + 10
+    text_info = image_info.copyright.split('，')
+    font = ImageFont.truetype("font/SourceHanSansSC-Regular.otf", 30)
     for index, text in enumerate(text_info):
-        # debug_print(text)
-        draw.text((x, y + index * 60), text, fill=(255, 255, 255),  font=font)
-        
+        w, h = font.getsize(text)
+        debug_print(text + ' %d, %d ' % (w, h))
+        y = y + h
+        draw.text((x, y), text, fill=(255, 255, 255),  font=font)
     im.save(image_file_name)
 
     return 0
@@ -111,6 +169,11 @@ def fetch():
         query_dict = edict(parse.parse_qs(parsed_tuple.query))
         filename = image.startdate + '.jpg'  # + query_dict.rf[0]
         l_filename = local_root + '/' + filename
+        
+        logs.append(filename)
+        logs.append(root + image.url)
+        logs.append(image.copyright)
+        
         # debug_print(filename)
         if not os.path.exists(l_filename):
             resp = requests.get(root + image.url)
@@ -119,16 +182,19 @@ def fetch():
                     f.write(resp.content)
                     f.flush()
                     f.close()
-                logs = '\r\n'.join([filename, root + image.url, image.copyright])
-                debug_print(logs)
-                write_log(logs)
                 draw_text_info_to_image(l_filename, image)
                 notify(filename, image.copyright)
+                
+                log_text = '\r\n'.join(logs)
+                write_log(log_text)
+
             else:
                 pass
         else:
             pass
-
+    
+    
+    # debug_print(log_text)
     return 0
 
 
